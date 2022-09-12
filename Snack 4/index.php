da542c6e003d680891026d093eeb2229ea2302eb
Matteo Susanna <matteo2784@gmail.com>
2022-09-12 14:23:07 +0200

    Fine snack 4

---
+++ Snack 4/index.php
@@ -0,0 +1,16 @@
+<?php
+    $numeriCasuali = [];
+
+    while(count($numeriCasuali) < 15){
+        $numeroGenerato = rand(1,15);
+
+        if(in_array($numeroGenerato, $numeriCasuali)){
+        }else{
+            $numeriCasuali[] = $numeroGenerato;
+        }
+
+    }
+    
+    var_dump($numeriCasuali);
+
+?>
